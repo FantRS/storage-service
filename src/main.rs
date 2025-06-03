2e2034f5a746c6c5fd6731cd76dbe3c4ba538e6c
5antUA <rostykkashper2@gmail.com>
2025-06-03 17:33:28 +0300

    updt

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,25 @@
-use std::env::home_dir;
-use serde::{Deserialize, Serialize};
-
 mod store;
+use crate::store::{load, save};
 
-// test
+use serde_json::to_string;
+use std::{env, path::Path};
+
+// testing foo
 fn main() {
+    let data = to_string(&[69, 1200]).unwrap();
 
-    let data = Person {
-        name: String::from("Sanya"),
-        age: 19,
-        is_male: true
-    };
+    let home_dir = env::home_dir().unwrap();
+    let path = Path::join(&home_dir, "Documents");
 
-    let home_dir = home_dir();
-    if let Some(path) = home_dir {
-        let path_str = path.to_str().unwrap();
-        let doc_path = store::combine_path(path_str, "Documents");
-        store::save(doc_path.as_str(), "test.txt", data);
-
-        let some: Person = store::load(&(doc_path + "/test.txt")).unwrap();
-        println!("{:?}", some);
+    match save("test_data.txt", &path, data) {
+        Ok(_) => println!("Saved data!"),
+        Err(why) => println!("{:?}", why),
     }
-}
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Person {
-    name: String,
-    age: i8,
-    is_male: bool
+    let path = Path::join(&path, "test_data.txt");
+
+    match load(&path) {
+        Ok(data) => println!("Data : {}", data),
+        Err(why) => println!("{:?}", why),
+    }
 }
--- src/store.rs
+++ src/store.rs
@@ -1,43 +1,37 @@
-use serde::{de::DeserializeOwned, Serialize};
-use serde_json::{to_string, from_str};
-use std::{fs::{self, File}, io::{Read, Write}, path::Path};
+use std::{
+    fs::File,
+    io::{self, Read, Write},
+    path::Path,
+};
 
-pub fn save<T>(path: &str, filename: &str, data: T)
-where
-    T : Serialize
-{
-    if !Path::new(&path).exists() {
-        _ = fs::create_dir(path);
+/// Saves serialized string data
+pub fn save(filename: &str, path: &Path, data: String) -> Result<(), io::Error> {
+    if !path.exists() {
+        return Err(io::Error::new(
+            io::ErrorKind::NotFound,
+            format!("Path is not exists or {:?} is not file.", path),
+        ));
     }
 
-    let path = combine_path(path, filename);
-    let json_data = to_string(&data).unwrap();
-    
-    let mut file = File::create(&path).unwrap();
-    _ = file.write_all(json_data.as_bytes());
+    let filepath = Path::join(path, filename.trim_start_matches('/'));
+    let mut file = File::create(&filepath)?;
+    _ = file.write_all(data.as_bytes());
+
+    Ok(())
 }
 
-pub fn load<T>(path: &str) -> Option<T>
-where 
-    T : DeserializeOwned
-{
-    let file = File::open(path);
-
-    if let Ok(mut file) = file {
-        let mut json_data = String::new();
-        _ = file.read_to_string(&mut json_data);
-
-        let res = from_str::<T>(json_data.as_str());
-
-        match res {
-            Ok(data) => return Some(data),
-            Err(_) => return None
-        };
+/// Loads serialized string data
+pub fn load(path: &Path) -> Result<String, io::Error> {
+    if !path.exists() {
+        return Err(io::Error::new(
+            io::ErrorKind::NotFound,
+            format!("Path is not exists or {:?} is not file.", path),
+        ));
     }
-    
-    None
-}
 
-pub fn combine_path(path1: &str, path2: &str) -> String {
-    format!("{}/{}", path1, path2)
+    let mut file = File::open(&path)?;
+    let mut result = String::new();
+    file.read_to_string(&mut result)?;
+
+    Ok(result)
 }
